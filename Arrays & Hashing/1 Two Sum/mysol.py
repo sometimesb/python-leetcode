f2ab350001d0650e6ac26e73bcb1b3874bf8d864
Bilal Zakaria <77695101+BilalZakaria@users.noreply.github.com>
2024-01-25 14:21:39 -0600

    finished 1

---
+++ Arrays & Hashing/1 Two Sum/mysol.py
@@ -0,0 +1,15 @@
+#Brute Force Method, O(n^2) time complexity.
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    answer.append(i)
+                    answer.append(j)
+            
+
+        return(answer)
---
+++ Arrays & Hashing/1 Two Sum/neetcode.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement],i]
+            numMap[nums[i]] = i
+        print(numMap)
