62a7c7d07ad40d4f6bc8b0f443159006a2c0ba4a
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-02-05 11:27:40 -0600

    finished 1984

---
+++ Two Pointers/1984 Minimum Difference Between Highest and Lowest of K Scores/mysol.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0 
+        right = k -1
+        res = float('inf')
+
+        while right < len(nums):
+            res = min(res, nums[right] - nums[left])
+
+            left+=1
+            right+=1
+
+        return res
